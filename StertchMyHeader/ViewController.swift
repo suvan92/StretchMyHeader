dc8e7c84ae6eccd0833fed134b0b747c9f21f0cf
Suvan <suvanramani@gmail.com>
2016-12-06 15:32:19 -0800

    Allow image resizing based on scrolling

--- StertchMyHeader/ViewController.swift
+++ StertchMyHeader/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-let customCellReuseIdentifier = "customCell"
+private let kCustomCellReuseIdentifier = "customCell"
+private let kTableHeaderHeight: CGFloat = 250.0
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate
+{
     
     // MARK: Properties
     @IBOutlet weak var tableView: UITableView!
+    var headerView: UIView!
     let items = [
         NewsItem(category: .World, summary: "Climate change protests, divestments meet fossil fuels realities"),
         NewsItem(category: .Europe, summary: "Scotland's 'Yes' leader says independence vote is 'once in a lifetime"),
@@ -29,16 +32,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setTableViewRowDimensions()
+        
+        // Set our tableView property to the tableView's header
+        headerView = tableView.tableHeaderView
+        
+        // Remove the tableView's control of it's header
+        tableView.tableHeaderView = nil
+        
+        // Re-add the subview that we have control over
+        tableView.addSubview(headerView)
+        
+        // Push contents of the tableView down by the height of the header (i.e. kTableViewHeaderHeight)
+        tableView.contentInset = UIEdgeInsetsMake(kTableHeaderHeight, 0, 0, 0)
+        
+        // Start with Header Completely Visible
+        tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
+        
+        updateHeaderView()
     }
     
     // MARK: TableView
+    
+    func updateHeaderView() {
+        var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: tableView.bounds.width, height: kTableHeaderHeight)
+        if tableView.contentOffset.y < -kTableHeaderHeight {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y
+        }
+        headerView.frame = headerRect
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateHeaderView()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: customCellReuseIdentifier, for: indexPath) as! CustomTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCustomCellReuseIdentifier, for: indexPath) as! CustomTableViewCell
         cell.newsItem = items[indexPath.row]
         return cell
     }
