4d19a8e461b3690c0df750196e50d4720a980092
Suvan <suvanramani@gmail.com>
2016-12-06 13:03:06 -0800

    Add newsItem property to CustomTableViewCell

--- StertchMyHeader/CustomTableViewCell.swift
+++ StertchMyHeader/CustomTableViewCell.swift
@@ -9,8 +9,24 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
+    // MARK: Properties
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var headlineLabel: UILabel!
+    
+    var newsItem: NewsItem? {
+        didSet {
+            if let item = newsItem {
+                categoryLabel.text = item.category.toString()
+                categoryLabel.textColor = item.category.toColor()
+                headlineLabel.text = item.summary
+            }
+            else {
+                categoryLabel.text = nil
+                headlineLabel.text = nil
+            }
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
